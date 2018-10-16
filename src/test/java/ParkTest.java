2039296c16e465e587900e0a09d6a9623c3941d1
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-10-16 15:42:37 +0100

    park created

---
+++ src/test/java/ParkTest.java
@@ -0,0 +1,22 @@
+import attractions.Park;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ParkTest {
+
+    Park park;
+
+    @Before
+    public void before() {
+        park = new Park("ParkInsideAPark");
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("ParkInsideAPark", park.getName());
+    }
+
+
+}
