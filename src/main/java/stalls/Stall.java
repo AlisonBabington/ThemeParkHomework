88f8a14f3a17c5038c8b02fd2e4b9caee9b07618
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-10-16 15:46:15 +0100

    stall abstract class commited

---
+++ src/main/java/stalls/Stall.java
@@ -0,0 +1,38 @@
+package stalls;
+
+public abstract class Stall {
+
+    private String name;
+    private String ownersName;
+    private int parkingSpot;
+
+    public Stall (String name, String ownersName, int parkingSpot) {
+        this.name = name;
+        this.ownersName = ownersName;
+        this.parkingSpot = parkingSpot;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwnersName() {
+        return ownersName;
+    }
+
+    public void setOwnersName(String ownersName) {
+        this.ownersName = ownersName;
+    }
+
+    public int getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(int parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+}
