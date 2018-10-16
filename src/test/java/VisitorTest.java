493bfbc0dcf339281648a741023f7bb75804aef6
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-10-16 15:22:14 +0100

    visitor class created

---
+++ src/test/java/VisitorTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class VisitorTest {
+
+    Visitor visitor;
+
+    @Before
+    public void before() {
+        visitor = new Visitor(17, 6.0, 200.00);
+    }
+
+    @Test
+    public void canGetAge() {
+        assertEquals(17, visitor.getAge());
+    }
+
+    @Test
+    public void canGetHeight() {
+        assertEquals(6.0, visitor.getHeight(), 0.01);
+    }
+
+    @Test
+    public void canGetMoney() {
+        assertEquals(200.00, visitor.getMoney(), 0.01);
+    }
+}
