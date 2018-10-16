b228891639e77fbf686a9e58849244a374b194e2
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-10-16 15:29:37 +0100

    rollercoaster and abstract attraction class created

---
+++ src/test/java/RollercoasterTest.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RollercoasterTest {
+
+    Rollercoaster rollercoaster;
+
+    @Before
+    public void before() {
+        rollercoaster = new Rollercoaster("Leviathan");
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Leviathan", rollercoaster.getName());
+    }
+}
