9f746b3004eef752cc215913c1937ed4661463ef
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-10-16 16:51:25 +0100

    dodgem using ITicketed interface

--- src/main/java/themeparks/attractions/Dodgem.java
+++ src/main/java/themeparks/attractions/Dodgem.java
@@ -1,8 +1,22 @@
 package themeparks.attractions;
 
-public class Dodgem extends Attraction{
+import themeparks.ITicketed;
+import themeparks.Visitor;
+
+public class Dodgem extends Attraction implements ITicketed {
 
     public Dodgem(String name) {
         super(name);
+    }
+
+    @Override
+    public double defaultPrice() {
+        return 4.50;
+    }
+
+    @Override
+    public double priceFor(Visitor visitor) {
+        if (visitor.getAge() < 12) return defaultPrice() / 2;
+        return defaultPrice();
     }
 }
--- src/test/java/themeparkstest/AttractionTest/DodgemTest.java
+++ src/test/java/themeparkstest/AttractionTest/DodgemTest.java
@@ -1,5 +1,6 @@
 package themeparkstest.AttractionTest;
 
+import themeparks.Visitor;
 import themeparks.attractions.Dodgem;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,14 +10,24 @@ import static org.junit.Assert.assertEquals;
 public class DodgemTest {
 
     Dodgem dodgem;
+    Visitor visitor;
+    Visitor visitor2;
 
     @Before
     public void before() {
         dodgem = new Dodgem("Dodgems");
+        visitor = new Visitor(17, 210, 200.00);
+        visitor2 = new Visitor(11, 190, 200.00);
     }
 
     @Test
     public void canGetName() {
         assertEquals("Dodgems", dodgem.getName());
     }
+
+    @Test
+    public void canCharge() { assertEquals(4.50, dodgem.priceFor(visitor), 0.01);}
+
+    @Test
+    public void canChargeHalfPriceForKids() {assertEquals(2.25, dodgem.priceFor(visitor2), 0.01);}
 }
